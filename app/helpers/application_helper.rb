ea0556534c6413ea5574484e447073d5d2672028
y503unavailable <y503unavailable@users.noreply.github.com>
2021-12-19 20:41:26 +0900

    Add Commonmark-2 (backport redmineorg-32424,except 21165 repo-entry.html.erb) #64

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1829,6 +1829,16 @@ module ApplicationHelper
     )
   end
 
+  # Returns the markdown formatter: markdown or common_mark
+  # ToDo: Remove this when markdown will be removed
+  def markdown_formatter
+    if Setting.text_formatting == "common_mark"
+      "common_mark"
+    else
+      "markdown"
+    end
+  end
+
   private
 
   def wiki_helper
